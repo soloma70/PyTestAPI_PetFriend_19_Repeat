5dfa510828314895ab74765da7a20a55e4309924
soloma70 <soloma70@gmail.com>
2021-10-18 13:45:27 +0300

    Добавлен блок тестов на проверку добавления питомцев с параметризацией по name, animal_type и age. Всего тестов 1053

--- tests/test_pet_friends_param.py
+++ tests/test_pet_friends_param.py
@@ -20,6 +20,19 @@ def get_api_key():
     # Проверяем что статус ответа в тесте = 200 и имя питомца соответствует заданному
     # assert pytest.status == 200
 
+# Вспомашательные функции для добавления различных строк
+def generate_string(n):
+   return "x" * n
+
+def russian_chars():
+   return 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+def chinese_chars():
+   return '的一是不了人我在有他这为之大来以个中上们'
+
+def special_chars():
+   return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
+
 
 # Блок тестов на проверку списка питомцев
 # Позитивный тест на получение списка питомцев с использованием параметризации
@@ -34,18 +47,6 @@ def test_get_all_pets_with_valid_key(filter):
    assert len(result['pets']) > 0
 
 # Негативный тест на получение списка питомцев с использованием параметризации
-def generate_string(n):
-   return "x" * n
-
-def russian_chars():
-   return 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-
-def chinese_chars():
-   return '的一是不了人我在有他这为之大来以个中上们'
-
-def special_chars():
-   return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
-
 @pytest.mark.parametrize("filter",
                         [generate_string(255), generate_string(1001), russian_chars(),
                         russian_chars().upper(), chinese_chars(), special_chars(), 123]
@@ -65,7 +66,32 @@ def test_get_all_pets_with_negative_filter(filter):
 # Для получения зеленых тестов проверяем pytest.status на код 500.
 
 
-# Блок тестов на проверку добавления питомцев
+# Блок тестов на проверку добавления питомцев с параметризацией по name, animal_type и age. Всего тестов 1053.
+@pytest.mark.parametrize("name", ['Матюся', '', generate_string(255), generate_string(1001), russian_chars()
+    , russian_chars().upper(), chinese_chars(), special_chars(), '123']
+    , ids=['my name', 'empty', '255 sym', '> 1000 symb', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+@pytest.mark.parametrize("animal_type", ['британец', '', generate_string(255), generate_string(1001), russian_chars()
+    , russian_chars().upper(), chinese_chars(), special_chars(), '123']
+    , ids=['my animal type', 'empty', '255 sym', '> 1000 sym', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+@pytest.mark.parametrize("age", ['9', '', '-1', '0', '1', '100', '1.5', '2147483647', '2147483648', special_chars()
+    , russian_chars(), russian_chars().upper(), chinese_chars()]
+    , ids=['my age', 'empty', 'negative', 'zero', 'min', '> max', 'float', 'int_max', 'int_max + 1', 'specials'
+    , 'russian', 'RUSSIAN', 'chinese'])
+def test_add_new_pet_simple(name, animal_type, age):
+    """Тест добавления нового питомца с с различными данными"""
+    # Добавляем питомца
+    pytest.status, result = pf.add_new_pet_simple(pytest.key, name, animal_type, age)
+    # Сверяем полученный ответ с ожидаемым результатом
+    # if name == '' or animal_type == '':
+    #     assert pytest.status == 400
+    # else:
+    assert pytest.status == 200
+    assert result['name'] == name
+    assert result['age'] == age
+    assert result['animal_type'] == animal_type
+
+
+
 def test_add_new_pet_with_valid_data(name='Матюся', animal_type='британец', age='9', pet_photo='images/cat11.jpg'):
     """Позитивный тест добавления нового питомца с корректными данными"""
     # Получаем полный путь изображения питомца и сохраняем в переменную pet_photo
